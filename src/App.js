4331a76b3bfe0dda674029ae9ae8b22d16d7012e
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2023-01-19 18:22:24 +1100

    Updated main page body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import NavBar from "./components/Navigation";
+import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Project";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <div>
+    <Header />
+
+    <main>
+    <About />
+    <Projects />
+    <Contact />
+    </main>
+
+    <ScrollToTop />
+    <Footer />
+</div>
   );
 }
 
